36346129ed6dcb746d858453def14547a0ae81d0
hadyanadam <hadyanadamn@gmail.com>
2020-02-21 15:36:39 +0700

    Speedometer Add

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import PostPage, UpdatePostPage
+from .views import PostPage, UpdatePostPage, AjaxGasView
 
 app_name='api'
 urlpatterns = [
+    path('ajax/<str:no_tiang>', AjaxGasView.as_view(), name='ajaxgasview'),
     path('', PostPage.as_view(), name='postpage'),
     path('<int:pk>', UpdatePostPage.as_view(), name='updatepostpage')
 ]
